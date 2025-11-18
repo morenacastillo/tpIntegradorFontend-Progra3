c2748abf3d68f5604829386451773793b7db255c
morenacastillo <moracastillo7@gmail.com>
2025-11-18 19:15:32 -0300

    estructura

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-console.log("hola");
+
+let contenedorProductos = document.getElementById("contenedor-productos")
+
+async function obtenerProductos() {
+    let url = "http://localhost:3000/products"
+    try{
+        let respuesta = await fetch("http://localhost:3000/products");
+        let respuestaFormato = await respuesta.json();
+        let productos = await respuestaFormato.payload;
+    
+        console.table(productos)
+
+        mostrarProductos(productos);
+
+    } catch (error) {
+        
+    }
+}
+
+function mostrarProductos(productos){
+    let htmlProductos = "";
+    productos.forEach(prod => {
+        htmlProductos += `
+        <div class="card-producto">
+            <img src="${prod.imagen}" alt="${prod.titulo}">
+            <h3>${prod.titulo}</h3>
+            <p>${prod.precio}</p>
+        </div>
+        `;
+        contenedorProductos.innerHTML = htmlProductos;
+    });
+}
+
+
+function init() {
+    obtenerProductos();
+}
+
+init();
